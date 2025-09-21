1853f3373656b3561aa995935638f8365eae9591
Anmolmahajn <mahajn1608@gmail.com>
2025-09-21 19:55:34 +0530

    Initial commit

---
+++ src/main/java/com/am/servlet/LogoutServlet.java
@@ -0,0 +1,15 @@
+package com.am.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        resp.sendRedirect("login.jsp");
+    }
+}
